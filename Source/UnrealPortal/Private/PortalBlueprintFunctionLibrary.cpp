ba8b122d171a536d4b2ec5847a7e47e3ca969f6b
iliaakm <iliaakm@gmail.com>
2025-02-02 23:42:53 +0300

    add unreal portal function library

---
+++ Source/UnrealPortal/Private/PortalBlueprintFunctionLibrary.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PortalBlueprintFunctionLibrary.h"
+
+bool UPortalBlueprintFunctionLibrary::IsPortalsOverlapped()
+{
+	return false;
+}
